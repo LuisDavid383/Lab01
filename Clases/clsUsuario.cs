04dd7c82be6bb5dbc917a71d09ee7478bb64f687
Luis David <luimosquito@upt.pe>
2025-10-01 15:10:24 -0500

    v5

--- Clases/clsUsuario.cs
+++ Clases/clsUsuario.cs
@@ -12,13 +12,15 @@ namespace PG02__LAB01_FICHEROS_LUIS_MOSQUITO.Clases
         public string nombre { get; set; }
         public string apellidoPaterno { get; set; }
         public string apellidoMaterno { get; set; }
+        public string tipo_documento { get; set; }
+        public string num_documento { get; set; }
+        public string celular { get; set; }
+        public string email { get; set; }
+        public string direccion { get; set; }
 
         public string mtdCadena(string codigo, string nombre, string apellidopaterno, string apellidomaterno)
         {
-            return codigo + "|" + nombre + "|" + apellidopaterno + "|" + apellidomaterno;
+            return codigo + "|" + nombre + "|" + apellidopaterno + "|" + apellidomaterno + "|" + tipo_documento + "|" + num_documento + "|" + celular + "|" + email + "|" + direccion;
         }
-
-
-        //agregar los demas campos
     }
 }
